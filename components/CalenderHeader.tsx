8636e0b523a73e2e1765a87ae2ce07066faec2d7
장성민 <aristatait@gmail.com>
2022-08-29 17:15:59 +0900

    feat: 달력 헤더 UI

---
+++ components/CalenderHeader.tsx
@@ -0,0 +1,45 @@
+import { format } from "date-fns";
+import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import { Box, Button, Grid, Stack, Typography } from "@mui/material";
+import { fontSize, spacing } from "@mui/system";
+
+interface CalendarHeaderProps {
+  currentMonth: Date | number;
+}
+
+const CalendarHeader = ({ currentMonth }: CalendarHeaderProps) => {
+  console.log(currentMonth);
+  return (
+    <Grid
+      container
+      columns={7}
+      sx={{
+        border: 1,
+        p: 2
+      }}
+    >
+      <Grid item xs={6}>
+        <Typography variant="h4" component="span">
+          {format(currentMonth, "MM")}월
+        </Typography>
+        <Typography component="span" sx={{ marginLeft: 1 }}>
+          {format(currentMonth, "yyyy")}년
+        </Typography>
+      </Grid>
+      <Grid item xs={1}>
+        <Stack
+          direction={"row"}
+          alignItems={"center"}
+          justifyContent={"space-between"}
+        >
+          <NavigateBeforeIcon sx={{ fontSize: 40 }} />
+          <Button>이번 달</Button>
+          <NavigateNextIcon sx={{ fontSize: 40 }} />
+        </Stack>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default CalendarHeader;
--- pages/calendar.tsx
+++ pages/calendar.tsx
@@ -1,10 +1,12 @@
+import CalendarHeader from "@components/CalenderHeader";
 import { NextPage } from "next";
 import React from "react";
 
 const CalendarPage: NextPage = () => {
+  const now = Date.now();
   return (
     <div>
-      <div>Header</div>
+      <CalendarHeader currentMonth={now} />
       <div>Days</div>
       <div>Body</div>
     </div>
