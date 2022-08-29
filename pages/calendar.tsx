319ace4cd0f8f39d45b7f30b8d67db3775c9ea29
장성민 <aristatait@gmail.com>
2022-08-29 15:22:33 +0900

    feat: 기본 틀

---
+++ pages/calendar.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from "next";
+import React from "react";
+
+const CalendarPage: NextPage = () => {
+  return (
+    <div>
+      <div>Header</div>
+      <div>Days</div>
+      <div>Body</div>
+    </div>
+  );
+};
+
+export default CalendarPage;
