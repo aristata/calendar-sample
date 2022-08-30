0804cdcc2977e1c68462234e669878599bf69eab
장성민 <aristatait@gmail.com>
2022-08-30 14:25:11 +0900

    feat: 헤더에 범주 추가

--- components/CalenderHeader.tsx
+++ components/CalenderHeader.tsx
@@ -2,8 +2,11 @@ import { format } from "date-fns";
 import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import TodayIcon from "@mui/icons-material/Today";
+import CircleIcon from "@mui/icons-material/Circle";
 import { Box, Button, Grid, Stack, Typography } from "@mui/material";
 import { fontSize, spacing } from "@mui/system";
+import { Height } from "@mui/icons-material";
+import { relative } from "path";
 
 interface CalendarHeaderProps {
   currentMonth: Date;
@@ -27,13 +30,40 @@ const CalendarHeader = ({
         p: 2
       }}
     >
-      <Grid item xs={6}>
+      <Grid item xs={1}>
         <Typography variant="h4" component="span">
           {format(currentMonth, "MM")}월
         </Typography>
         <Typography component="span" sx={{ marginLeft: 1 }}>
           {format(currentMonth, "yyyy")}년
         </Typography>
+      </Grid>
+      <Grid item xs={5} sx={{ position: "relative" }}>
+        <Stack
+          direction={"row"}
+          spacing={2}
+          alignItems={"flex-end"}
+          sx={{ position: "absolute", height: "100%", paddingBottom: 1 }}
+        >
+          <span>
+            <CircleIcon
+              sx={{ width: 16, height: 16, color: "blue", marginRight: 1 }}
+            />
+            매출액
+          </span>
+          <span>
+            <CircleIcon
+              sx={{ width: 16, height: 16, color: "gray", marginRight: 1 }}
+            />
+            입금 예정 정산액
+          </span>
+          <span>
+            <CircleIcon
+              sx={{ width: 16, height: 16, color: "#69f0ae", marginRight: 1 }}
+            />
+            입금 완료 정산액
+          </span>
+        </Stack>
       </Grid>
       <Grid item xs={1}>
         <Stack
